2358a346ac594f97de2f62e9a84066a568cf5155
zenAndroid <21260658+zenAndroid@users.noreply.github.com>
2019-12-21 18:41:58 -0500

    Add Track semi-implementation.
    I'm useless with C++, for now.

---
+++ Track.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <Track.h>
+#include <Cell.h>
+#include <Train.h>
+
+/* Constructors {{{1 */
+ /* DEFAULT CONSTRUCTOR {{{2 */
+Track::Track()
+{
+    std::cout << "Call ought never to happen" << std::endl;
+    exit(1);
+}
+/* 2}}} */
+/* "Good" CONSTRUCTOR {{{2 */
+Track::Track(int trackLength, Cell fStation, Cell sStation, Cell tStation, int fLocation, int sLocation, int tLocation){
+    // Forgive me Allah for what i'm about to do ...
+    //
+    // TODO: Fill this up
+}
+
+/* 2}}} */
+/* 1}}} */
--- Track.h
+++ Track.h
@@ -12,6 +12,9 @@ private:
 public:
     Track();
     Track(int trackLength, Cell fStation, Cell sStation, Cell tStation, int fLocation, int sLocation, int tLocation); // So much for generality ..
+    // This Constructor is a fucking disgrace, however i'll run with this at first then i assume i'll set up a map or something ...
+    // Although I don't even fucking know if that'd be a decent way yo solve it.
+    // Still, better than fucking hardcoding it in the constructor, i bet.
 };
 
 #endif /* TRACK_H */
